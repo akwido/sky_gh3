7265ebf24ee77b8a9c5c964b1c1aa1d0a9275620
akwido <root@debian.loc>
2015-11-30 01:07:58 +0300

    Сохраниние 001

---
+++ tom.php
@@ -0,0 +1,26 @@
+<?php
+
+
+echo "=========== tom==========";
+echo "<br>";
+echo " just change php file for view working git ";
+echo "<br>";
+
+//$a=5;
+//$b=20;
+//$c=a+b;
+$f= "stroke";
+echo "dIGITAL:";
+echo "<br>";
+//echo  $c ;
+//echo  "$c";
+//print($c);
+print($f);
+
+
+
+
+?>
+
+
+
