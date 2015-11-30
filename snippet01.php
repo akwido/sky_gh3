9f08e8227cbdb7ab76bccef27d7409184eb0899d
akwido <root@debian.loc>
2015-11-30 13:03:26 +0300

    my_save08

--- snippet01.php
+++ snippet01.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>
+         Snippet01
+      </title>
+ <style type="text/css">
+   body {background: LightSalmon;}
+  </style>
+
+
+   </head>
+   <body>
+
 <?php
 
 echo "..............Snippet01............";
@@ -5,3 +20,5 @@ echo "<br>"
 
 
 ?>
+</body>
+</html>
--- tom.php
+++ tom.php
@@ -1,10 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>tom</title>
+	<style>
+		body{background:yellow}
+	</style>
+   </head>
+   <body>
 
+<?php
 
 echo "=========== tom==========";
 echo "<br>";
 echo " just change php file for view working git ";
-echo "<br>";
+echo "<br><br>";
 
 //$a=5;
 //$b=20;
